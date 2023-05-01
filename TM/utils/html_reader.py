40e18611f3c18b7cad94ea99becfd91eaf604d6f
good-programmer <129273886+good-programmer@users.noreply.github.com>
2023-05-01 16:45:36 +0800

    Revert "update html reader"
    This reverts commit 73c645a8ad070e3560ec3d97d9f5a4381ffb0169.

--- TM/utils/html_reader.py
+++ TM/utils/html_reader.py
@@ -6,4 +6,7 @@ def html_reader(route):
         route = "index"
     html_path = f"views/{route}.html"
 
-    if os.path.ex
+    if os.path.exists(html_path):
+        with open(html_path, "r") as html:
+            return 200, html.read()
+    return 404, "Page not found"

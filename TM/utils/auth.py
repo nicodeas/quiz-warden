63fec61651a3d082c782668ee4d97360583b855b
good-programmer <129273886+good-programmer@users.noreply.github.com>
2023-05-03 08:44:10 +0800

    login_user now uses data from json

--- TM/utils/auth.py
+++ TM/utils/auth.py
@@ -1,13 +1,16 @@
 import hashlib
+import json
 from user import users
 
 
 # Verifies user credentials exist against pre-registered users
 def login_user(username, password):
-    # TODO: update this to traverse and verify against pre-registered users
-    if (username == 'admin' and password == 'admin') or (username == 'jack' and password == 'jack'):
-        return True
-    return False
+    with open('data/users.json', 'r') as f:
+        data = json.load(f)
+        for user in data['users']:
+            if user['username'] == username and user['password'] == password:
+                return True
+        return False
 
 # Hashes username to serve as session_id for user
 def generate_session_id(username):
@@ -16,4 +19,6 @@ def generate_session_id(username):
 
 # Checks that session_token in header is same as session_id in User class
 def is_valid_session(session_token):
+    if session_token in users:
+        print("accessing page as " + users[session_token].username)
     return session_token in users

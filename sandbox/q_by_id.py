afca9e963335bcb62bc8b05545caa745693166f8
good-programmer <129273886+good-programmer@users.noreply.github.com>
2023-05-09 22:54:29 +0800

    refactor TM socket request

--- sandbox/q_by_id.py
+++ sandbox/q_by_id.py
@@ -1,4 +1,10 @@
 import socket
+from request_processors import *
+
+
+# debug: req variables for q_id and session_token
+Q_TO_GET = 3
+SESSION_TOKEN = 888
 
 HOST = "localhost"  # replace with server address
 PORT = 8080  # replace with server port
@@ -7,42 +13,27 @@ PORT = 8080  # replace with server port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
-Q_TO_GET = 0
-SESSION_TOKEN = 888
-
 # request needs to contain a valid keyword else there will be an error
-#req = f"GET_QUESTION_BY_ID|{Q_TO_GET}"
-req = f"GENERATE_QUESTIONS|{SESSION_TOKEN}"
+req = f"GET_QUESTION_BY_ID|{Q_TO_GET}"
+#req = f"GENERATE_QUESTIONS|{SESSION_TOKEN}"
 s.send(req.encode())
 
-qb_response =""
-# receive and print the response from the server
-while True:
-    data = s.recv(1024)
-    if not data:
-        break
-    qb_response+=data.decode()
-
-    #qb_response = data.decode()
+qb_response, image_data = receive_data(s)
 print(qb_response)
-if "GET_QUESTION_BY_ID" in req:
-    question_parts = qb_response.split('&')
-    question = {
-        'id': int(question_parts[0]),
-        'language': question_parts[1],
-        'type': question_parts[2],
-        'text': question_parts[3]
-    }
-    if question['type'] == 'CHOICE':
-        question['choices'] = question_parts[4].split('^')
-    if question['type'] == 'IMAGE':
-        question['image'] = question_parts[4]
-        # TODO: add file write
+
+if "GENERATE_QUESTIONS" in req:
+    # array holding order of q_ids from a server
+    questions = process_generated_questions(qb_response)
+    print(questions)
+elif "MARK_QUESTION_BY_ID" in req:
+    pass
+elif "GET_QUESTION_BY_ID" in req:
+    # dict storing question fields; image is written to directory
+    question = process_question(qb_response, image_data)
     print("TM end after sorting question into dict:")
     print(question)
-elif "GENERATE_QUESTIONS" in req:
-    questions = [question for question in qb_response.split('\n') if question]
-    print(questions)
+elif "HEALTH_CHECK" in req:
+    pass
 
 # close the socket
 s.close()
---
+++ sandbox/request_processors.py
@@ -0,0 +1,60 @@
+import io
+
+def receive_data(s):
+    # flag denoting when stream of image data begins
+    IMAGE_DATA_FLAG = b'$'
+    receiving_image_data = False
+    # store question fields
+    qb_response = b""
+    # store image data
+    image_data = b""
+
+    # receive response from the server
+    while True:
+        data = s.recv(io.DEFAULT_BUFFER_SIZE)
+        if not data:
+            break
+        # check for image data flag
+        if not receiving_image_data:
+            image_index = data.find(IMAGE_DATA_FLAG)
+            # reading image data!
+            if image_index != -1:
+                qb_response += data[:image_index]
+                image_data += data[image_index + len(IMAGE_DATA_FLAG):]
+                receiving_image_data = True
+            else:
+                # no image flag; read as regular response
+                qb_response += data
+        else:
+            image_data += data
+    return qb_response, image_data
+
+# stores question fields in a dict; writes image_data to file if it exists
+def process_question(qb_response, image_data):
+    question_parts = qb_response.decode().split('&')
+    # only receives fields to display question to user; marking handled by QB
+    question = {
+        'id': int(question_parts[0]),
+        'language': question_parts[1],
+        'type': question_parts[2],
+        'text': question_parts[3]
+    }
+    if question['type'] == 'CHOICE':
+        question['choices'] = question_parts[4].split('^')
+    elif question['type'] == 'IMAGE':
+        image_info =  question_parts[4].split('$')
+        question['image'] = image_info[0]
+        process_image_data(image_data, question['image'])
+    return question
+
+def process_image_data(image_data, filename):
+    # TODO: naming convention and directory for images
+    # possibly just name after question?
+    # could images just be stored on TM?
+    with open(filename, "wb") as f:
+        f.write(image_data)
+
+# reads stream of q_ids and stores in list
+def process_generated_questions(qb_response):
+    questions = [question for question in qb_response.decode().split('&') if question]
+    return questions

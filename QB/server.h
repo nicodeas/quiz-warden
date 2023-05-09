837a7bf4a4cd36176cb7e229149cc959cd68d9b8
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-05-09 09:41:43 +0800

    debug variable

--- QB/server.h
+++ QB/server.h
@@ -65,6 +65,7 @@ extern Question *QUESTION_BANK[QUESTION_BANK_SIZE];
 extern int NUM_QUESTIONS; // write to this when building question bank to keep
                           // track of max index we can go; not sure if this will
                           // be required
+extern bool debug;
 
 // server functions in server.c
 extern void buildQuestionBank(); // TODO: setup function on server startup to

0584dbf328ee0ec1311a5f998d4b88179bba5070
Jack Sun <jsun1590@gmail.com>
2023-05-14 21:45:48 +0800

    init tm on login

--- TM/routes/api/login.py
+++ TM/routes/api/login.py
@@ -1,5 +1,5 @@
 import os
-from classes.user import *
+from classes.user import User, users
 from utils.auth import *
 
 from ..base import BaseRoute
@@ -19,20 +19,23 @@ class Login(BaseRoute, route="api"):
         if login_user(username, password):
             session_id = generate_session_id(username)
             if session_id not in users:
-                user = User(username, session_id)                
+                user = User(username, session_id)
                 users[session_id] = user
 
                 data = {}
                 if os.path.exists("data/session.json"):
                     with open("data/session.json", "r") as f:
                         data = json.load(f)
-
                 if session_id not in data:
                     data[session_id] = {}
-
+                else:
+                    user.init_tm(
+                        data[session_id]["questions"],
+                        data[session_id]["current_question"],
+                        data[session_id]["completed"],
+                    )
                 with open("data/session.json", "w") as f:
                     json.dump(data, f, indent=2)
-            # Add session_id to header and redirect to index
 
             cookie = f"session_token={session_id}; Path=/; Max-Age=99999999;"
             headers = {"Set-Cookie": cookie}

aa3a7ca7333a81e0fce445177d6c07d01c4ed545
Jack Sun <jsun1590@gmail.com>
2023-05-18 23:30:35 +0800

    remove log

--- TM/static/js/quiz/renderResult.js
+++ TM/static/js/quiz/renderResult.js
@@ -1,7 +1,7 @@
 import { getState } from "../utils/getState.js";
 
 const state = await getState();
-console.log(state);
+
 if (state.completed) {
   document.getElementById("total-marks").innerText = state.totalMarks;
 } else {

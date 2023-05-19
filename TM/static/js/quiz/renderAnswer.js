d675e027ac5fb04513a0f5c4be3134598f5a2f73
Jack Sun <jsun1590@gmail.com>
2023-05-19 11:53:21 +0800

    fix js

--- TM/static/js/quiz/renderAnswer.js
+++ TM/static/js/quiz/renderAnswer.js
@@ -1,7 +1,7 @@
 export const renderAnswer = (data, type) => {
   const answerEl = document.getElementById("answer-container");
   if (type === "IMAGE") {
-    answerEl.innerHTML = `<imgsrc="data:image/png;base64, ${data.answer}" />`;
+    answerEl.innerHTML = `<img src="data:image/png;base64, ${data.answer}" />`;
   } else {
     answerEl.innerHTML = `<p>The answer is: <span id="correct-answer"></span></p>`;
     document.getElementById("correct-answer").innerText = atob(data.answer);
